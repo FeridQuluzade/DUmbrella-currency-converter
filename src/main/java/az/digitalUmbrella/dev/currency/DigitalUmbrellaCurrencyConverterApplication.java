22c3496ce57a495f9866c38aa294d30205631934
FeridQuluzade <feridquluzade2002@gmail.com>
2022-07-15 20:13:22 +0400

    initial commit

---
+++ src/main/java/az/digitalUmbrella/dev/currency/DigitalUmbrellaCurrencyConverterApplication.java
@@ -0,0 +1,13 @@
+package az.digitalUmbrella.dev.currency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalUmbrellaCurrencyConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalUmbrellaCurrencyConverterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/digitalUmbrella/dev/currency/DigitalUmbrellaCurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package az.digitalUmbrella.dev.currency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalUmbrellaCurrencyConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
